bfb05991c3abd96bbf4f655112497b56c120ffe9
mdelzotto <madelzotto@comcast.net>
2019-04-01 17:05:52 -0400

    Challenge 3, list alphabetically.

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -84,7 +84,10 @@ console.log(inventory[lastCar]['car_make'], inventory[lastCar]['car_model']);
 let carModels = [];
 console.log();
 
-
+for (let i=0; i < inventory.length; i++) {
+	carModels.push(inventory[i]["car_model"]);
+}
+console.log(carModels.sort());
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
