8c24cadbc402e060e6c582460c46e9ac85a3281f
mdelzotto <madelzotto@comcast.net>
2019-04-01 19:10:47 -0400

    Finished Objects Folder, Challenge 1-2

--- assignments/objects.js
+++ assignments/objects.js
@@ -31,7 +31,11 @@ const kennan = {
   "name": "Kennan",
   "email": "kdiben1@tinypic.com",
   "gender": "M"
+  "speak": function(){
+		console.log('Hello, my name is Kennan!');
+  }
 }
+
 
 const kevin = {
   "id": 3,
@@ -52,6 +56,9 @@ const antonietta = {
   "name": "Antonietta",
   "email": "adaine5@samsung.com",
   "gender": "F"
+  "multiplyNums": function(num1, num2) {
+		return num1 * num2;
+	}
 }
 
 // ==== Challenge 2: Reading Object Data ====
@@ -79,7 +86,7 @@ console.log(antonietta['gender']);
 
 // ==== Challenge 3: Object Methods ====
 // Give Kennan the ability to say "Hello, my name is Kennan!" Use the console.log provided as a hint.
-// console.log(kennan.speak());
+ //console.log(kennan.speak());
 
 // Antonietta loves math, give her the ability to multiply two numbers together and return the product. Use the console.log provided as a hint.
 //console.log(antonietta.multiplyNums(3,4));
